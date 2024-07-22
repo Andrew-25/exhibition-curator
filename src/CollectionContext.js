e56d0c2f4a0ac8fac000fbc1118c078bb455c04f
Andrew-25 <andrewoozs@hotmail.com>
2024-07-22 15:26:57 +0100

    querying works

--- src/CollectionContext.js
+++ src/CollectionContext.js
@@ -3,17 +3,7 @@ import { createContext, useState } from "react";
 export const CollectionContext = createContext();
 
 export const CollectionProvider = ({ children }) => {
-  const [state, setState] = useState([
-    {
-      id: 'example',
-      name: 'example name',
-      description: 'example description',
-      imageLink: 'broken link',
-      imageAlt: 'example image alt',
-      museum: 'example museum',
-      curatorWebsite: true
-    }
-  ]);
+  const [state, setState] = useState([])
 
   return (
     <CollectionContext.Provider value={{ state, setState }}>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,20 +1,33 @@
 import { useEffect, useState } from "react";
 import { fetchScienceMuseum, fetchVam } from "../apis/fetch";
 import Result from "./Result";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Search = () => {
     let navigate = useNavigate()
-    const [searchResults, setSeachResults] = useState([])
+    let location = useLocation()
+    const [searchResults, setSearchResults] = useState([])
+    const [pageNo, setPageNo] = useState(1)
 
     useEffect(() => {
+        let scienceSearch = `/search/objects?page[size]=10&page[number]=${pageNo - 1}`
+        let vamSearch = `/objects/search?page_size=10&images_exist=1&page=${pageNo}`
+        
+        if (location.pathname.slice(8) !== 'all') {
+            scienceSearch += `&q=${location.pathname.slice(8)}`
+            vamSearch += `&q=${location.pathname.slice(8)}`
+        }
+        
         const fetchRequests = async () => {
-            const scienceData = await fetchScienceMuseum('/search/objects?page[size]=10')
-            const vamData = await fetchVam('/objects/search?page_size=10&images_exist=1')
-            setSeachResults([...scienceData, ...vamData.records])
+            const scienceData = await fetchScienceMuseum(scienceSearch)
+            const vamData = await fetchVam(vamSearch)
+            setSearchResults([...scienceData, ...vamData.records])
         }
         fetchRequests()
-    }, [])
+    }, [pageNo])
+
+    const prevPage = () => setPageNo(pageNo - 1)
+    const nextPage = () => setPageNo(pageNo + 1)
 
     if (searchResults.length === 0) {
         return (
@@ -28,10 +41,13 @@ const Search = () => {
               <h1>Search results</h1>
               <button onClick={() => navigate('/')}>Home</button>
               <button onClick={() => navigate('/collection')}>Your Collection</button>
+              <button hidden={pageNo < 2} onClick={() => prevPage()}>Prev</button>
+              <p>Page: {pageNo}</p>
+              <button onClick={() => nextPage()}>Next</button>
               <ul>
                 {searchResults.map((res) => {
-                    if (res.systemNumber) return <Result result={res} listKey={res.systemNumber}/>
-                    else return <Result result={res} listKey={res.id}/>
+                    if (res.systemNumber) return <Result result={res} listKey={res.systemNumber} key={res.systemNumber}/>
+                    else return <Result result={res} listKey={res.id} key={res.id}/>
                 })}
               </ul>
             </div>
